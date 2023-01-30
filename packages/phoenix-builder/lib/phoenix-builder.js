69c8abb7a2eac3dc22184fe10ac040ab7fbd6698
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-01-30 15:36:58 +0530

    Added the CSS modules postcss processor for compilation

--- packages/phoenix-builder/lib/phoenix-builder.js
+++ packages/phoenix-builder/lib/phoenix-builder.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 const rollup = require('rollup');
+const postcss = require('rollup-plugin-postcss');
 const path = require('path');
 const resolve = require('@rollup/plugin-node-resolve').default;
 const babel = require('@rollup/plugin-babel').default;
 
 const currentWorkingPath = process.cwd();
+
 const { main, name } = require(path.join(currentWorkingPath, 'package.json'));
 
 const inputPath = path.join(currentWorkingPath, main);
@@ -17,6 +19,10 @@ const inputOptions = {
   input: inputPath,
   external: ['react'],
   plugins: [
+    postcss({
+      // Key configuration
+      modules: true,
+    }),
     resolve(),
     babel({
       presets: ['@babel/preset-env', '@babel/preset-react'],
