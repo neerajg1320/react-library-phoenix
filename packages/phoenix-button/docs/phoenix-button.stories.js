a1ee01e784e3128ff1d49710c5351985cc8191d1
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-01-30 15:49:06 +0530

    Added support for a component variant

--- packages/phoenix-button/docs/phoenix-button.stories.js
+++ packages/phoenix-button/docs/phoenix-button.stories.js
@@ -2,4 +2,11 @@ import React from 'react';
 import { Button } from '../lib/phoenix-button';
 
 export default { title: 'Button' };
+
 export const primary = () => <Button>Hello Button</Button>;
+
+export const secondary = () => (
+    <div style={{ background: '#1652f0', padding: 12 }}>
+      <Button variant="secondary">Hello Button</Button>
+    </div>
+);
